7f05c71902fda9c8cf247f42b3f6847e2cb2d7a3
bjoern <bjoern.stockleben@rbb-online.de>
2013-05-27 19:27:27 +0200

    updated the tracer to output xml

--- 3dv_server/js/3dv_tracer.js
+++ 3dv_server/js/3dv_tracer.js
@@ -43,17 +43,17 @@ $(document).ready(
 
 			document.onmousedown = function () {
 				mouseDown = true;
-				console.log("mousedown");
-			}
+				//console.log("mousedown");
+			};
 
 			document.onmouseup = function () {
 				mouseDown = false;
-			}
+			};
 
 			document.onmousemove = function (event){
 				mouseX = event.pageX;
 				mouseY = event.pageY;
-			}
+			};
 
 			// create popcorn instance
 			pop = Popcorn("#video", {
@@ -63,7 +63,7 @@ $(document).ready(
 
 			pop.play();
 
-			pop.cue( 2, function() {
+			pop.cue( 1, function() {
 				console.log( this.duration() );
 				this.pause(0);
 				this.playbackRate(PLAYBACK_RATE);
@@ -81,45 +81,55 @@ function prepare_trackpoints(media_duration){
 		frameRate : FRAMERATE
 	});
 
-	media_duration_frames = media_duration*FRAMERATE
-	trackpoints = new Array();
+	media_duration_frames = media_duration*FRAMERATE;
+	trackpoints = [];
 	
 	for (var i = 0; i < media_duration_frames; i = i + FRAME_STEP){
 		//console.log("giving cues");
-		pop.cue(i/FRAMERATE, function(options) {
-			if (mouseDown) {
-				console.log("cueMouseDown");
-				
-				var x1 = mouseX;
-				var y1 = mouseY;
-				console.log("break 1");
-				var tp = new TrackPoint(x1,y1,this.currentTime(),OBJECT_ID);
-				console.log(tp.toXML());
-				trackpoints.push(tp);
-				console.log("break 2");
-				var canvas = document.getElementById('marker_canvas');
-				console.log("break 3");
-				if (canvas.getContext) {
-					console.log("break 4");
-					var context = canvas.getContext('2d');
-					// Erase canvas
-					//context.canvas.height = context.canvas.height;
-					context.fillStyle = 'rgba(127,127,127,0.4)';
-					console.log("break 5");
-					context.fillRect(x1-5, y1-5, 10, 10);
-				}
-
-			} else {
-				console.log("cueMouseUp");
-				var canvas = document.getElementById('marker_canvas');
-				if (canvas.getContext) {
-					var context = canvas.getContext('2d');
-					// Erase canvas
-					//context.canvas.height = context.canvas.height;
-				}
-			}
-		});	
+		pop.cue(i/FRAMERATE, create_trackpoint);	
 
 	}
 
+}
+
+function create_trackpoint(options) {
+	//console.log("called create_trackpoint");
+	if (mouseDown) {
+		console.log("cueMouseDown");
+		
+		var x1 = mouseX;
+		var y1 = mouseY;
+		console.log("break 1");
+		var tp = new TrackPoint(x1,y1,this.currentTime(),OBJECT_ID);
+		console.log(tp.toXML());
+		trackpoints.push(tp);
+		console.log("break 2");
+		var canvas = document.getElementById('marker_canvas');
+		console.log("break 3");
+		if (canvas.getContext) {
+			console.log("break 4");
+			var context = canvas.getContext('2d');
+			// Erase canvas
+			//context.canvas.height = context.canvas.height;
+			context.fillStyle = 'rgba(127,127,127,0.4)';
+			console.log("break 5");
+			context.fillRect(x1-5, y1-5, 10, 10);
+		}
+
+	} else {
+		console.log("cueMouseUp");
+		var canvas = document.getElementById('marker_canvas');
+		if (canvas.getContext) {
+			var context = canvas.getContext('2d');
+			// Erase canvas
+			//context.canvas.height = context.canvas.height;
+		}
+	}
+}	
+
+function print_xml(){
+	console.log("print_xml called");
+	$.each(trackpoints, function(index, value){
+		$("#result").append(value.toXML());
+	});
 }
