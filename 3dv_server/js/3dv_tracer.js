0c921dad18b308d5d8a06204d76d6b1406785f9c
bjoern <bjoern.stockleben@rbb-online.de>
2013-06-07 14:13:03 +0200

    added bootstrap resources to create better tracing interface

--- 3dv_server/js/3dv_tracer.js
+++ 3dv_server/js/3dv_tracer.js
@@ -1,108 +1,153 @@
 console.log('huhu');
 var FRAMERATE = 25;
-//Determine Accuracy of tracking
+// Determine Accuracy of tracking
 var FRAME_STEP = 2;
-//set playback speed for easier tracking
+// set playback speed for easier tracking
 var PLAYBACK_RATE = 0.3;
 
-//standard object id, maybe dynamic later
+// standard object id, maybe dynamic later
 var OBJECT_ID = 10;
 
-//buffers for mouseactions
+// the object that is currently worked on
+var current_trackobject_index = 0;
+
+var trackobjects = [];
+
+// buffers for mouseactions
 var mouseDown = false;
 var mouseX = -1;
 var mouseY = -1;
 
-var trackpoints;
+// object
+function TrackObject(title, description, author, link, thumb) {
+	this.trackpoints = [];
+	this.title = title;
+	this.description = description;
+	this.author = author;
+	this.link = link;
+	this.thumb = thumb;
 
-//tracking object
-function TrackPoint(x1, y1, frame, object_id) {
+	this.toXML = toXML;
+	
+	function toXML(object_id) {
+		return '<link object="' + object_id + '" dest="' + this.link + '">'
+				+ '<title>' + this.title + '</title>' + '<author>'
+				+ this.author + '</author>' + '<image>' + this.thumb
+				+ '</image>' + '<description>' + this.description
+				+ '</description>' + '</link>';
+	}
+}
+
+// tracking point
+function TrackPoint(x1, y1, frame) {
 	console.log("creating TrackPoint");
 	this.x1 = x1;
 	this.y1 = y1;
 	this.frame = frame;
-	this.object_id = object_id;
 
 	this.toString = toString;
 	this.toXML = toXML;
-	
+
 	function toString() {
-		return "TrackPoint " + x1 + "," + y1 + "," + frame + "," + object_id;
+		return "TrackPoint " + x1 + "," + y1 + "," + frame;
 	}
 
-	function toXML() {
-		return '<frame id="'+frame+'"><BoundingBox id="'+object_id+'"><Min Y="'+(y1-20)+'" X="'+(x1-20)+'" />'
-		+'<Max Y="'+(y1+20)+'" X="'+(x1+20)+'" /></BoundingBox></frame>';
+	function toXML(object_id) {
+		return '<frame id="' + frame + '"><BoundingBox id="' + object_id
+				+ '"><Min Y="' + (y1 - 20) + '" X="' + (x1 - 20) + '" />'
+				+ '<Max Y="' + (y1 + 20) + '" X="' + (x1 + 20)
+				+ '" /></BoundingBox></frame>';
 	}
 	console.log("finished creating TrackPoint");
-	
+
 }
 
-$(document).ready(
-		function() {
+$(document).ready(function() {
 
-			document.onmousedown = function () {
-				mouseDown = true;
-				//console.log("mousedown");
-			};
+	document.onmousedown = function() {
+		mouseDown = true;
+		// console.log("mousedown");
+	};
 
-			document.onmouseup = function () {
-				mouseDown = false;
-			};
+	document.onmouseup = function() {
+		mouseDown = false;
+	};
 
-			document.onmousemove = function (event){
-				mouseX = event.pageX;
-				mouseY = event.pageY;
-			};
+	document.onmousemove = function(event) {
+		mouseX = event.pageX;
+		mouseY = event.pageY;
+	};
 
-			// create popcorn instance
-			pop = Popcorn("#video", {
-				frameAnimation : true,
-				frameRate : FRAMERATE
-			});
-
-			pop.play();
-
-			pop.cue( 1, function() {
-				console.log( this.duration() );
-				this.pause(0);
-				this.playbackRate(PLAYBACK_RATE);
-				var duration = this.duration();
-				this.destroy();
-				prepare_trackpoints(duration);
-			});
-
-		});
-
-function prepare_trackpoints(media_duration){
+	trackobjects.push(new TrackObject('title', 'description', 'author', 'link', 'thumb'));
+	current_trackobject_index = 0;
+	update_ui();
+	
 	// create popcorn instance
 	pop = Popcorn("#video", {
 		frameAnimation : true,
 		frameRate : FRAMERATE
 	});
 
-	media_duration_frames = media_duration*FRAMERATE;
-	trackpoints = [];
+	pop.play();
+
+	pop.cue(1, function() {
+		console.log(this.duration());
+		this.pause(0);
+		this.playbackRate(PLAYBACK_RATE);
+		var duration = this.duration();
+		this.destroy();
+		prepare_trackpoints(duration);
+	});
+
+});
+
+function update_ui(){
+	var object = trackobjects[current_trackobject_index];
+	$("#object_data #object_name").val(object.title);
+	$("#object_data #object_description").val(object.description);
+	$("#object_data #object_link").val(object.link);
+	$("#object_data #object_image").val(object.thumb);
 	
-	for (var i = 0; i < media_duration_frames; i = i + FRAME_STEP){
-		//console.log("giving cues");
-		pop.cue(i/FRAMERATE, create_trackpoint);	
+	$("#object_list").empty().append('<li class="nav-header">Traced Objects</li>');
+	
+	$.each(trackobjects, function(object_index, value) {
+		console.log('update_ui '+object_index);
+		if (object_index == current_trackobject_index) $("#object_list").append('<li class="active"><a href="#">'+value.title+'</a></li>');
+		else $("#object_list").append('<li><a href="#">'+value.title+'</a></li>');		
+	});
+	
+	$("#object_list").append('<li class="divider"></li><li><a href="#">add a new object</a></li>');
+}
+
+function prepare_trackpoints(media_duration) {
+	// create popcorn instance
+	pop = Popcorn("#video", {
+		frameAnimation : true,
+		frameRate : FRAMERATE
+	});
+
+	media_duration_frames = media_duration * FRAMERATE;
+	trackpoints = [];
+
+	for ( var i = 0; i < media_duration_frames; i = i + FRAME_STEP) {
+		// console.log("giving cues");
+		pop.cue(i / FRAMERATE, create_trackpoint);
 
 	}
 
 }
 
 function create_trackpoint(options) {
-	//console.log("called create_trackpoint");
+	// console.log("called create_trackpoint");
 	if (mouseDown) {
 		console.log("cueMouseDown");
-		
+
 		var x1 = mouseX;
 		var y1 = mouseY;
 		console.log("break 1");
-		var tp = new TrackPoint(x1,y1,this.currentTime(),OBJECT_ID);
+		var tp = new TrackPoint(x1, y1, this.currentTime(), OBJECT_ID);
 		console.log(tp.toXML());
-		trackpoints.push(tp);
+		trackobjects[current_trackobject_index].trackpoints.push(tp);
 		console.log("break 2");
 		var canvas = document.getElementById('marker_canvas');
 		console.log("break 3");
@@ -110,10 +155,10 @@ function create_trackpoint(options) {
 			console.log("break 4");
 			var context = canvas.getContext('2d');
 			// Erase canvas
-			//context.canvas.height = context.canvas.height;
+			// context.canvas.height = context.canvas.height;
 			context.fillStyle = 'rgba(127,127,127,0.4)';
 			console.log("break 5");
-			context.fillRect(x1-5, y1-5, 10, 10);
+			context.fillRect(x1 - 5, y1 - 5, 10, 10);
 		}
 
 	} else {
@@ -122,14 +167,26 @@ function create_trackpoint(options) {
 		if (canvas.getContext) {
 			var context = canvas.getContext('2d');
 			// Erase canvas
-			//context.canvas.height = context.canvas.height;
+			// context.canvas.height = context.canvas.height;
 		}
 	}
-}	
+}
 
-function print_xml(){
+function print_xml() {
 	console.log("print_xml called");
-	$.each(trackpoints, function(index, value){
-		$("#result").append(value.toXML());
+	var output_xml = '<?xml version="1.0" encoding="utf-8"?><nle_data><links>';
+	$.each(trackobjects, function(object_index, value) {
+		output_xml = output_xml + value.toXML(object_index);
 	});
+
+	output_xml = output_xml + '</links><frames>';
+
+	$.each(trackobjects, function(object_index, value) {
+		$.each(value.trackpoints, function(index, value) {
+			output_xml = output_xml + value.toXML(object_index);
+		});
+	});
+	output_xml = output_xml + '</frames>';
+	console.log(output_xml);
+	return output_xml;
 }
